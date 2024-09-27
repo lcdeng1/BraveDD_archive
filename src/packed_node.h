65a199d3c0e180069dba392e402cb2984868a18c
lcdeng <lcdeng@iastate.edu>
2024-09-26 23:14:56 -0500

    [Remove] packed_node.cc

--- src/packed_node.h
+++ src/packed_node.h
@@ -61,11 +61,31 @@ class BRAVE_DD::PackedNode {
     /*-------------------------------------------------------------*/
     public:
     /*-------------------------------------------------------------*/
-    /// Default construction for Rex
-    PackedNode();
-    PackedNode(Node& node);
-    PackedNode(Mxnode& node);
-    ~PackedNode();
+    PackedNode() {}
+    PackedNode(Node& node) {
+        info = (uint32_t*)malloc(5 * sizeof(uint32_t));
+        for (int i=0; i<5; i++) {
+            info[i] = 0;
+        }
+        values = 0;
+    }
+    PackedNode(Mxnode& node) {
+        info = (uint32_t*)malloc(8 * sizeof(uint32_t));
+        for (int i=0; i<8; i++) {
+            info[i] = 0;
+        }
+        info[1] |= NODE_TYPE_MASK;
+        values = (uint64_t*)malloc(3 * sizeof(uint64_t));
+        for (int i=0; i<3; i++) {
+            values[i] = 0;
+        }
+    }
+    ~PackedNode() {
+        for (int i=0; info[i]; i++) info[i] = 0;
+        free(info);
+        for (int i=0; values[i]; i++) values[i] = 0;
+        free(values);
+    }
 
     /// Methods =====================================================
     /**
@@ -195,7 +215,7 @@ class BRAVE_DD::PackedNode {
      * 
      * @return Node 
      */
-    inline Node unpackNode() {
+    inline Node unpackNode() const {
         //
         Node unpacked;
         //
@@ -206,12 +226,13 @@ class BRAVE_DD::PackedNode {
      * 
      * @return Mxnode 
      */
-    inline Mxnode unpackMxnode() {
+    inline Mxnode unpackMxnode() const {
         //
         Mxnode unpacked;
         //
         return unpacked;
     }
+    
     /**
      *  Fill in a packed node from a given unpacked node
      *  Sets next to 0 and clears the marked bits
@@ -271,8 +292,6 @@ class BRAVE_DD::PackedNode {
         }
         return 1;
     }
-
-
 
     /*-------------------------------------------------------------*/
     private:
