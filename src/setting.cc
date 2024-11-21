58db238983378b16cdc91517225cf6d209b4ea7d
lcdeng <lcdeng@iastate.edu>
2024-11-21 00:27:37 -0600

    [Update] regex

--- src/setting.cc
+++ src/setting.cc
@@ -42,32 +42,32 @@ ForestSetting::ForestSetting(const std::string& bdd, const unsigned numVals)
 :domain(numVals), range(BOOLEAN, VOID), reductions(REX), flags(ONE, COMP)
 {
     // BDDs
-    if (std::regex_match(bdd, std::regex("^(R|r)(E|e)(X|x)(B|b)(D|d)(D|d)$"))) {
+    if (std::regex_match(bdd, std::regex("rexbdd", std::regex_constants::icase))) {
         // setting for RexBDD
         encodingType = TERMINAL;
         mergeType = PUSH_UP;
         name = "RexBDD";
-    } else if (std::regex_match(bdd, std::regex("^(Q|q)(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("qbdd", std::regex_constants::icase))) {
         // setting for QBDD
-    } else if (std::regex_match(bdd, std::regex("^(F|f)(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("fbdd", std::regex_constants::icase))) {
         // setting for FBDD
-    } else if (std::regex_match(bdd, std::regex("^(M|m)(T|t)(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("mtbdd", std::regex_constants::icase))) {
         // setting for MTBDD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(\\+)?(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(\\+)?bdd", std::regex_constants::icase))) {
         // setting for EV+BDD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(\\+)?(?:(\\%)|mod)(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(\\+)?(?:(\\%)|mod)bdd", std::regex_constants::icase))) {
         // setting for EV%BDD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(?:(\\*)|star)(B|b)(D|d)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(?:(\\*)|star)bdd", std::regex_constants::icase))) {
         // setting for EV*BDD
     }
     // MxDs
-    else if (std::regex_match(bdd, std::regex("^(M|m)(T|t)(B|b)(M|m)(X|x)(D|d)$"))) {
+    else if (std::regex_match(bdd, std::regex("mtbmxd", std::regex_constants::icase))) {
         // setting for MTBMxD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(\\+)?(B|b)(M|m)(X|x)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(\\+)?bmxd", std::regex_constants::icase))) {
         // setting for EV+BMxD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(\\+)?(?:(\\%)|mod)(B|b)(M|m)(X|x)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(\\+)?(?:(\\%)|mod)bmxd", std::regex_constants::icase))) {
         // setting for EV%BMxD
-    } else if (std::regex_match(bdd, std::regex("^(E|e)(V|v)(?:(\\*)|star)(B|b)(M|m)(X|x)(D|d)$"))) {
+    } else if (std::regex_match(bdd, std::regex("ev(?:(\\*)|star)bmxd", std::regex_constants::icase))) {
         // setting for EV*BMxD
     } else {
         // Unknown predefined BDD or BMxD
