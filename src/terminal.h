6a68b72d8138cda0cc53790605d1e3b094b4bdc5
lcdeng <lcdeng@iastate.edu>
2024-11-21 12:25:36 -0600

    [Update]

--- src/terminal.h
+++ src/terminal.h
@@ -7,54 +7,90 @@
 namespace BRAVE_DD {
     // ******************************************************************
     // *                                                                *
-    // *                     Terminal NodeHandle                        *
+    // *                        Terminal Handle                         *
     // *                                                                *
     // ******************************************************************
     /**
-     * @brief Check if the given EdgeHandle is target to terminal.
-     * Note: terminal nodes are not stored, they are represented 
-     *       by a special NodeHandle value.
+     * EdgeHandle pointing to terminal nodes
+     * Assuming terminal value can only be:
+     *          INT, FLOAT, -∞, +∞, UNDEF, OMEGA.
+     * header (9 bits): bit 63: 1: if terminal is float value;
+     *                  bit 62: 1: if terminal is int value;
+     *                  bit 61: 1: if terminal is special value;
      * 
-     * @param handle        The given EdgeHandle.
-     * @return true         – If it's terminal.
-     * @return false        – If not.
+     * In Node storage: informatin in target NodeHandle, ForestSetting tells the meaning
+     * 
      */
-    static inline bool isTerminal(const EdgeHandle handle) {
-        return unpackLevel(handle) == 0;
-    }
+    
     /**
      * @brief Get the terminal value for the given EdgeHandle.
      * Note: it would check if the given handle is terminal, 
-     *       and return -1 if not.
+     *       and exit if not.
      * 
      * @param handle        The given EdgeHandle.
-     * @return EdgeValue     – Output termianl value wrapper.
+     * @return Value     – Output termianl value wrapper.
      */
-    static inline EdgeValue getTerminalValue(const EdgeHandle handle) {
-        // TBD
-        EdgeValue val(0);
+    static inline Value getTerminalValue(const EdgeHandle& handle) {
+        if(unpackLevel(handle)>0) {
+            std::cout << "[BRAVE_DD] ERROR!\t No value for nonterminal node!"<< std::endl;
+            exit(0);
+        }
+        Value val(0);
+        NodeHandle data = unpackNode(handle);
+        if (handle & FLOAT_VALUE_FLAG_MASK) {
+            // float value
+            float value = *reinterpret_cast<float*>(&data);
+            val.setValue(&value, FLOAT);
+        } else if (handle & INT_VALUE_FLAG_MASK) {
+            // int value
+            int value = *reinterpret_cast<int*>(&data);
+            val.setValue(&value, INT);
+        } else if (handle & SPECIAL_VALUE_FLAG_MASK) {
+            // special value
+            SpecialValue value = *reinterpret_cast<SpecialValue*>(&data);
+            val.setValue(&value, VOID);
+        } else {
+            // unknown value
+            std::cout << "[BRAVE_DD] ERROR!\t Unknown value for terminal node!"<< std::endl;
+            exit(0);
+        }
         return val;
     }
+    
     /**
-     * @brief Make the special NodeHandle for terminal Ω. 
-     * This is usually used for edge valued BDDs.
+     * @brief Make a plain EdgeHandle target to terminal node, by giving the terminal value and type.
      * 
-     * @return NodeHandle   – Output terminal node handle.
+     * @param type 
+     * @param value 
+     * @return EdgeHandle 
      */
-    static inline NodeHandle makeTerminal() {
-        // TBD 
-        return 0;
+    static inline EdgeHandle makeTerminal(const ValueType type, const void* value) {
+        if (type == DOUBLE || type == LONG) {
+            std::cout << "[BRAVE_DD] ERROR!\t Unsupported data type! It can only be INT/FLOAT."<< std::endl;
+            exit(0);
+        }
+        EdgeHandle handle = 0;
+        NodeHandle node = 0;
+        if (type == INT) {
+            handle |= INT_VALUE_FLAG_MASK;
+            int target = *((int*) value);
+            node = *reinterpret_cast<NodeHandle*>(&target);
+        } else if (type == FLOAT) {
+            handle |= FLOAT_VALUE_FLAG_MASK;
+            float target = *((float*) value);
+            node = *reinterpret_cast<NodeHandle*>(&target);
+        } else {
+            handle |= SPECIAL_VALUE_FLAG_MASK;
+            SpecialValue target = *((SpecialValue*) value);
+            node = *reinterpret_cast<NodeHandle*>(&target);
+        }
+        packTarget(handle, node);
+        return handle;
     }
-    /**
-     * @brief Make the special NodeHandle for termianl with 
-     * a given value.
-     * 
-     * @param val           The given terminal value.
-     * @return NodeHandle   – Output terminal node handle.
-     */
-    static inline NodeHandle makeTerminal(EdgeValue val) {
-        // TBD 
-        return 0;
+    template <typename T>
+    static inline EdgeHandle makeTerminal(const ValueType type, const T& value) {
+        // return makeTerminal(type, reinterpret_cast<void*>(const_cast<T*>(&value)));
+        return makeTerminal(type, static_cast<const void*>(&value));
     }
 
 }; // end of namespace
