264eb4805606e4344031148bd27fd73ee2b4d53f
lcdeng <lcdeng@iastate.edu>
2025-10-27 11:24:55 -0500

    [Update] examples

---
+++ examples/00_hello_world.cc
@@ -0,0 +1,12 @@
+/* 
+ * This is a basic example intended to output version information of the library.
+ *
+ * Author: Lichuan Deng
+ */
+
+#include "brave_dd.h"
+
+int main () {
+    using namespace BRAVE_DD;
+    printInfo();
+}
--- examples/01_logic_expression.cc
+++ examples/01_logic_expression.cc
@@ -1,74 +1,93 @@
-#include "brave_dd.h"
+/* 
+ * This is an usage example of building logic expression in a BDD representation
+ * 
+ * Author: Lichuan Deng
+ */
 
-using namespace BRAVE_DD;
+ #include "brave_dd.h"
 
-int main () {
-    printInfo();
+bool isVisual = 0;
 
+void processArgs(int argc, const char** argv)
+{
+    for (int i=1; i<argc; i++) {
+        if ('-' == argv[i][0]) {
+            if (strcmp("-v", argv[i])==0) {
+                isVisual = 1;
+                continue;
+            }
+        }
+    }
+}
+
+int main (int argc, const char** argv) {
+    using namespace BRAVE_DD;
+    processArgs(argc, argv);
     /**
-     * Forest Setting Construction 1
-     * Note: this will construct the setting with a given 
-     *       number of variables and default BDD specifications.
-     */
-    // ForestSetting* setting1 = new ForestSetting(10);
-    ForestSetting setting1(10);
-    /* Configure here (any setting order) */
-    setting1.setNumVars(5);                    // Number of variables
-    setting1.setRangeType(FINITE);             // Range type
-    setting1.setValType(INT);                  // Value type
-    setting1.setMaxRange(6);                   // Max range, N
-    setting1.setNegInf(0);                     // Negative infinity
-    setting1.setPosInf(0);                     // Positive infinity
-    setting1.setUnDef(0);                      // Undefined
-    setting1.setEncodeMechanism(EDGE_PLUS);    // Encoding mechanism
-    setting1.setDim(1);                        // Dimension
-    setting1.setReductionType(FULLY);          // Reudction type
-    setting1.addReductionRule(RULE_AH0);
-    // ^ may add or delete rules here          // Add/delete reduction rules
-    setting1.setSwapType(NO_SWAP);             // Swap flag
-    setting1.setCompType(COMP);                // Complement flag
-    setting1.setMergeType(PUSH_UP);            // Merge type (this will be removed)
-    setting1.setName("v5f6-CEVBDD");           // Name
-
-
-    /**
-     * [Optional] Forest Setting Construction 2
-     * (name of predefined BDD, number of variables, is relation)
+     * Forest Setting Construction
+     * (name of predefined BDD, number of variables)
      * Note: this will construct the setting as predefined BDD
-     *       with the given name.
      */
-    // ForestSetting* setting2 = new ForestSetting("RexBDD", 10);
-    ForestSetting setting2("RexBDD", 10);
-    
-    /**
-     * Forest construction
-     * 
-     */
+    ForestSetting setting(PredefForest::FBDD, 4);
+    /* Declare and construct Forest */
+    Forest* forest = new Forest(setting);
+    /* Declare BDD roots attached to the Forest */
+    Func x1(forest), x2(forest), x3(forest), x4(forest);
+    /* Initialize BDD roots as variables */
+    x1.variable(1);
+    x2.variable(2);
+    x3.variable(3);
+    x4.variable(4);
+    /* Build the target logic expression */
+    Func target = (!x4 & !x1) | (x3 & x2);
+
+    if (isVisual) {
+        /* Build BDD in DOT language for visualization */
+        // this will generate a .gv file and compile it to the corresponding .pdf file
+        DotMaker dot2(forest, "02_logic_expression_example_FBDD");
+        dot2.buildGraph(target);
+        dot2.runDot("pdf");
+    }
+    /* Report information */
+    int align = 36;
+    std::cout << std::left << std::setw(align) << "BDD name: " << setting.getName() << std::endl;
+    std::cout << std::left << std::setw(align) << "Number of variables: " << setting.getNumVars() << std::endl;
+    std::cout << std::left << std::setw(align) << "Number of nodes: " << forest->getNodeManUsed(target) << std::endl;
+    long numOnes = 0;
+    apply(CARDINALITY, target, numOnes);
+    std::cout << std::left << std::setw(align) << "Number of assignments point to 1: " << numOnes << std::endl;
+
+    /* Declare and construct Forest */
+    ForestSetting setting1(PredefForest::REXBDD, 4);
+    /* Declare and construct Forest */
     Forest* forest1 = new Forest(setting1);
-    Forest* forest2 = new Forest(setting2);
+    Func y1(forest1), y2(forest1), y3(forest1), y4(forest1);
+    y1.variable(1);
+    y2.variable(2);
+    y3.variable(3);
+    y4.variable(4);
+    target = (!y4 & !y1) | (y3 & y2);
 
-    /**
-     * Some Operations
-     * 
-     */
+    if (isVisual) {
+        /* Build BDD in DOT language for visualization */
+        // this will generate a .gv file and compile it to the corresponding .pdf file
+        DotMaker dot2(forest1, "02_logic_expression_example_RexBDD");
+        dot2.buildGraph(target);
+        dot2.runDot("pdf");
+    }
 
-    // Roots
-    Func root1(forest1), root2(forest1);
-    root1.constant(5);
-    root2.variable(5);
-    root1 += root2;
-
-    // Forests
-    
-    // more operations TBD
-
+    /* Report information */
+    align = 36;
+    std::cout << std::left << std::setw(align) << "BDD name: " << setting1.getName() << std::endl;
+    std::cout << std::left << std::setw(align) << "Number of variables: " << setting1.getNumVars() << std::endl;
+    std::cout << std::left << std::setw(align) << "Number of nodes: " << forest1->getNodeManUsed(target) << std::endl;
+    numOnes = 0;
+    apply(CARDINALITY, target, numOnes);
+    std::cout << std::left << std::setw(align) << "Number of assignments point to 1: " << numOnes << std::endl;
 
     /**
      * Clean Everything
-     * 
      */
+    delete forest;
     delete forest1;
-    delete forest2;
-
-    return 0;
 }
