c0783aa1621e926b8970849958bfcdca8925928b
lcdeng <lcdeng@iastate.edu>
2025-10-30 10:56:24 -0500

    [Update] test

--- tests/00_test_setting.cc
+++ tests/00_test_setting.cc
@@ -1,59 +1,99 @@
-#include "brave_dd.h"
+#include "setting.h"
 
 using namespace BRAVE_DD;
 
 int main(int argc, char** argv) {
-    printInfo();
     std::string bdd;
     if (argc == 1) bdd = "rexbdd";
     else bdd = argv[1];
 
     // basic test case
-    unsigned numVals = 10;
-    // RangeType rangeType = FINITE;
-    // ValueType valueType = INT;
-    // int maxRange = 20;
-    // bool hasNegInf = 0;
-    // bool hasPosInf = 0;
-    // bool hasUnDef = 0;
-    // EncodeMechanism encode = TERMINAL;
-    // int dim = 1;
-    // ReductionType reductionType = FULLY;
-    // SwapSet swapType = ONE;
-    // CompSet CompSet = NO_COMP;
+    Level numVals = 10;
+    RangeType rangeType = FINITE;
+    ValueType valueType = INT;
+    int maxRange = 20;
+    bool hasNegInf = 0;
+    bool hasPosInf = 0;
+    bool hasUnDef = 0;
+    EncodeMechanism encode = TERMINAL;
+    int dim = 1;
+    ReductionType reductionType = FULLY;
+    SwapSet swapType = ONE;
+    CompSet compSet = NO_COMP;
+    MergeType mergeType = PUSH_DOWN;
+    std::string name = "v5f6-EVBDD";
 
     // default setting
     ForestSetting setting1(bdd, numVals);
-
-    // build forest
-    Forest forest1(setting1);
+    // change setting1
+    setting1.setNumVars(numVals);               // Number of variables
+    setting1.setRangeType(rangeType);           // Range type
+    setting1.setValType(valueType);             // Value type
+    setting1.setMaxRange(maxRange);             // Max range, N
+    setting1.setNegInf(hasNegInf);              // Negative infinity
+    setting1.setPosInf(hasPosInf);              // Positive infinity
+    setting1.setUnDef(hasUnDef);                // Undefined
+    setting1.setEncodeMechanism(encode);        // Encoding mechanism
+    setting1.setDim(dim);                       // Dimension
+    setting1.setReductionType(reductionType);   // Reudction type
+    // setting1.addReductionRule(RULE_I0);
+    // setting1.addReductionRule(RULE_I1);
+    setting1.setSwapType(swapType);             // Swap flag
+    setting1.setCompType(compSet);              // Complement flag
+    setting1.setMergeType(mergeType);           // Merge type (this will be removed)
+    setting1.setName(name);                     // Name
     
-    // change setting1 totally different from forest1
-    setting1.setNumVars(5);                    // Number of variables
-    setting1.setRangeType(FINITE);             // Range type
-    setting1.setValType(INT);                  // Value type
-    setting1.setMaxRange(6);                   // Max range, N
-    setting1.setNegInf(1);                     // Negative infinity
-    setting1.setPosInf(0);                     // Positive infinity
-    setting1.setUnDef(0);                      // Undefined
-    setting1.setEncodeMechanism(EDGE_PLUS);    // Encoding mechanism
-    setting1.setDim(2);                        // Dimension
-    setting1.setReductionType(FULLY);          // Reudction type
-    setting1.addReductionRule(RULE_I0);
-    setting1.addReductionRule(RULE_I1);
-    // ^ may add or delete rules here          // Add/delete reduction rules
-    setting1.setSwapType(NO_SWAP);             // Swap flag
-    setting1.setCompType(NO_COMP);             // Complement flag
-    setting1.setMergeType(PUSH_DOWN);          // Merge type (this will be removed)
-    setting1.setName("v5f6-EVBDD");            // Name
-    setting1.output(std::cout, 0);
-    forest1.getSetting().output(std::cout, 0);
-
-    if (forest1.getSetting().getNumVars() == setting1.getNumVars()) {
-        std::cout<< "Error!" << std::endl;
-        exit(1);
+    // check
+    bool hasError = 0;
+    if (setting1.getNumVars() != numVals) {
+        std::cerr << "[Error]: number of variables" << std::endl;
+        hasError = 1;
     }
-
-
-    return 0;
+    if (setting1.getRangeType() != rangeType) {
+        std::cerr << "[Error]: range type" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getValType() != valueType) {
+        std::cerr << "[Error]: value type" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getMaxRange() != (unsigned long)maxRange) {
+        std::cerr << "[Error]: max range" << std::endl;
+        hasError = 1;
+    }
+    if ((setting1.hasNegInf() != hasNegInf)
+        || (setting1.hasPosInf() != hasPosInf)
+        || (setting1.hasUnDef() != hasUnDef)) {
+        std::cerr << "[Error]: special values" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getEncodeMechanism() != encode) {
+        std::cerr << "[Error]: encode mechanism" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.isRelation() != (dim > 1)) {
+        std::cerr << "[Error]: dim" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getReductionType() != reductionType) {
+        std::cerr << "[Error]: reduction type" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getSwapType() != swapType) {
+        std::cerr << "[Error]: swap type" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getCompType() != compSet) {
+        std::cerr << "[Error]: complement set" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getMergeType() != mergeType) {
+        std::cerr << "[Error]: merge type" << std::endl;
+        hasError = 1;
+    }
+    if (setting1.getName() != name) {
+        std::cerr << "[Error]: name" << std::endl;
+        hasError = 1;
+    }
+    return hasError;
 }
