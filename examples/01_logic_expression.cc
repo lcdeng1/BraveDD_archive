4a63628b29dbb179ddbfe780ee04e1412dee9ead
lcdeng <lcdeng@iastate.edu>
2026-04-22 13:49:37 -0500

    [Sync] updated files from upstream: examples/01_logic_expression.cc examples/02_queens.cc examples/03_queens_cover.cc

--- examples/01_logic_expression.cc
+++ examples/01_logic_expression.cc
@@ -39,7 +39,7 @@ int main (int argc, const char** argv) {
     x3.variable(3);
     x4.variable(4);
     /* Build the target logic expression */
-    Func target = ((x4) & (x1)) | (x3 & x2);
+    Func target = ((!x4) & (!x1)) | (x3 & x2);
 
     if (isVisual) {
         /* Build BDD in DOT language for visualization */
